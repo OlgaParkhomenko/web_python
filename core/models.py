b3d3af8ae7897c0ed8e122fa24fa7df343ec2580
OlgaParkhomenko <opshop98@gmail.com>
2021-12-30 23:39:48 +0200

    HW15 Initial commit

---
+++ core/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Base(models.Model):
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(Base):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    image = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Like(Base):
+    post = models.ForeignKey(Post, null=False, blank=False, on_delete=models.CASCADE)
+    status = models.BooleanField(null=True, blank=True, default=None)
+
+    def __str__(self):
+        if self.status:
+            return 'Like'
+        elif self.status == 0:
+            return 'Dislike'
+        else:
+            return 'Deleted'
--- files_test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-def files_tree():
-    return os.system("tree")
-
-
-files_tree()
